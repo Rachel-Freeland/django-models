3cddd2261f1668e30d3a85417ea3ec32fcedbce3
Rachel Freeland <rachel.freeland1@gmail.com>
2022-05-24 22:21:29 -0500

    App is working but only has 2 tests

---
+++ README.md
@@ -0,0 +1,18 @@
+# LAB - Class 27
+## Project: Django Models
+### Author: Rachel Freeland
+### Links and Resources
+***
+### Setup
+***
+### How to initialize/run your application (where applicable)
+***
+### How to use this library (where applicable)
+***
+### Tests
+* How do you run tests?
+  * Use `python manage.py test` to run the tests
+* Any tests of note?
+  * No
+* Describe any tests that you did not complete, skipped, etc
+  * Unable to test the snack_detail page
--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+import snacks.views
+
+
+class SnacksTests(TestCase):
+    def test_list_page_status_code(self):
+        url = reverse("snack_list")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse("snack_list")
+        res = self.client.get(url)
+        self.assertTemplateUsed(res, "snack_list.html")
+        self.assertTemplateUsed(res, "base.html")
+
+    # def test_detail_page_status_code(self):
+    #     url = reverse("snack_detail", args=arr.pk)
+    #     res = self.client.get(url)
+    #     self.assertEqual(res.status_code, 200)
+
+    # def test_detail_page_template(self):
+    #     url = reverse("snack_detail", args=[1, 2, 3, 4])
+    #     res = self.client.get(url)
+    #     self.assertTemplateUsed(res, "snack_detail.html")
+    #     self.assertTemplateUsed(res, "base.html")
+
+
